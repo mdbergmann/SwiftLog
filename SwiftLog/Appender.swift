d6c1f36f67fb80344845841f41ac376be24b6569
Manfred Bergmann <manfred@everyoneprint.com>
2015-09-18 09:08:26 +0200

    Swift 2 compatibility.

--- SwiftLog/Appender.swift
+++ SwiftLog/Appender.swift
@@ -18,23 +18,18 @@ public class ConsoleAppender: NSObject, Appender {
     public override init() {}
     
     private func computePattern(level: Level, loggerName: String, message: String, functionName: String) -> String {
-        var outText = pattern.stringByReplacingOccurrencesOfString("[d]", withString: NSDate().description, options: NSStringCompareOptions.LiteralSearch, range: nil)
-        outText = outText.stringByReplacingOccurrencesOfString("[l]", withString: level.description(), options: NSStringCompareOptions.LiteralSearch, range: nil)
-        outText = outText.stringByReplacingOccurrencesOfString("[c]", withString: loggerName, options: NSStringCompareOptions.LiteralSearch, range: nil)
-        outText = outText.stringByReplacingOccurrencesOfString("[m]", withString: functionName, options: NSStringCompareOptions.LiteralSearch, range: nil)
-        outText = outText.stringByReplacingOccurrencesOfString("[M]", withString: message, options: NSStringCompareOptions.LiteralSearch, range: nil)
-        outText.extend("\n")
+        var outText = pattern.stringByReplacingOccurrencesOfString("[d]", withString:NSDate().description, options:NSStringCompareOptions.LiteralSearch, range:nil)
+        outText = outText.stringByReplacingOccurrencesOfString("[l]", withString:level.description(), options:NSStringCompareOptions.LiteralSearch, range:nil)
+        outText = outText.stringByReplacingOccurrencesOfString("[c]", withString:loggerName, options:NSStringCompareOptions.LiteralSearch, range:nil)
+        outText = outText.stringByReplacingOccurrencesOfString("[m]", withString:functionName, options:NSStringCompareOptions.LiteralSearch, range:nil)
+        outText = outText.stringByReplacingOccurrencesOfString("[M]", withString:message, options:NSStringCompareOptions.LiteralSearch, range:nil)
+        outText.appendContentsOf("\n")
         
         return outText
     }
     
     public func append(level: Level, loggerName: String, message: String, functionName: String) {
-        print(computePattern(level, loggerName:loggerName, message:message, functionName:functionName))
-        /*
-        let handle = createHandle()
-        handle.writeData(computePattern(level, loggerName: loggerName, message: message).dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!)
-        handle.closeFile()
-        */
+        print(computePattern(level, loggerName:loggerName, message:message, functionName:functionName), terminator: "")
     }
 
     private func createHandle() -> NSFileHandle { return NSFileHandle.fileHandleWithStandardOutput() }
@@ -48,11 +43,9 @@ public class FileAppender: ConsoleAppender {
     }
     
     override public func append(level: Level, loggerName: String, message: String, functionName: String) {
-        let text = computePattern(level, loggerName:loggerName, message:message, functionName:functionName)
-        
         let handle = createHandle()
         handle.seekToEndOfFile()
-        handle.writeData(computePattern(level, loggerName: loggerName, message:message, functionName:functionName).dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!)
+        handle.writeData(computePattern(level, loggerName:loggerName, message:message, functionName:functionName).dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)!)
         handle.closeFile()
     }
 
@@ -64,6 +57,6 @@ public class FileAppender: ConsoleAppender {
             NSFileManager.defaultManager().createFileAtPath(path!, contents: nil, attributes: nil)
         }
         
-        return NSFileHandle(forWritingToURL:fileUrl!, error:nil)!
+        return try! NSFileHandle(forWritingToURL:fileUrl!)
     }
 }
